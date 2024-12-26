7bfb97fe14380b2a74526266b4912b99a40989b3
anabebrastar <anathemastar@gmail.com>
2024-12-26 18:54:27 +0700

    реализация функций ввода

---
+++ funcs.py
@@ -0,0 +1,40 @@
+import random
+
+selected_task = None
+matrix = None
+array1 = None
+array2 = None
+array = None
+target_sum = None
+result = None
+
+def input_data():
+    global matrix, array1, array2, array, target_sum, result
+    result = None
+    if selected_task == '1':
+        matrix = input_matrix()
+    elif selected_task == '2':
+        array1, array2 = input_large_numbers()
+    elif selected_task == '3':
+        array, target_sum = input_array_and_sum()
+    else:
+        print("Сначала выберите задание.")
+
+def input_matrix():
+    n = int(input("Введите количество строк матрицы: "))
+    m = int(input("Введите количество столбцов матрицы: "))
+    matrix = []
+    for i in range(n):
+        row = list(map(int, input(f"Введите {m} элементов для строки {i+1}: ").split()))
+        matrix.append(row)
+    return matrix
+
+def input_large_numbers():
+    array1 = list(map(int, input("Введите первое большое число: ").split()))
+    array2 = list(map(int, input("Введите второе большое число: ").split()))
+    return array1, array2
+
+def input_array_and_sum():
+    array = list(map(int, input("Введите массив: ").split()))
+    target_sum = int(input("Введите число: "))
+    return array, target_sum
